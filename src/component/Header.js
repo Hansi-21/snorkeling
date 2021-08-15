652f65ce34134f18821c1115d4b7114488164aa7
Hansi <hansaninimeshika89@gmail.com>
2021-08-15 08:19:10 +0530

    snorkeling

---
+++ src/component/Header.js
@@ -0,0 +1,66 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
+import React from "react";
+
+function Header() {
+  return (
+    <>
+      <div className="flex flex-wrap py-2">
+        <div className="w-full ">
+          <nav
+            style={{ backgroundColor: "#055052" }}
+            className="relative flex flex-wrap items-center justify-between px-2 py-3"
+          >
+            <div className="container px-1 mx-auto flex flex-wrap items-center justify-between">
+              <div className="w-full relative flex justify-between lg:w-auto px-4 lg:static lg:block lg:justify-start">
+                <a
+                  className="text-sm font-bold leading-relaxed inline-block mr-4 py-2 whitespace-nowrap uppercase text-white"
+                  href="#home"
+                >
+                  Snorkeling Turtles In Mirissa
+                </a>
+              </div>
+              <div>
+                <ul className="flex flex-col lg:flex-row list-none lg:ml-auto">
+                  <li className="nav-item">
+                    <a
+                      className="px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug text-white hover:opacity-75"
+                      href="#about"
+                    >
+                      About
+                    </a>
+                  </li>
+                  <li className="nav-item">
+                    <a
+                      className="px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug text-white hover:opacity-75"
+                      href="#book"
+                    >
+                      Book
+                    </a>
+                  </li>
+                  <li className="nav-item">
+                    <a
+                      className="px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug text-white hover:opacity-75"
+                      href="#reviews"
+                    >
+                      Reviews
+                    </a>
+                  </li>
+                  <li className="nav-item">
+                    <a
+                      className="px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug text-white hover:opacity-75"
+                      href="#contact"
+                    >
+                      Contact
+                    </a>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </nav>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Header;
