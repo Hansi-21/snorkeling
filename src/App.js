a7c7ec4ee07ead4e56c1eb7059c1ab669fffcb5b
Hansi-21 <hansaninimeshika89@gmail.com>
2021-08-15 09:30:58 +0530

    snorkeling

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import "./App.css";
 import Header from "./component/Header";
 import Main from "./component/Main";
+import Footer from "./component/Footer";
 
 function App() {
   return (
     <div className="App">
      <Header/>
      <Main/>
+     <Footer/>
     </div>
   );
 }
